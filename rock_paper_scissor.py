e2beb3dbfbdc479556c50782dc2d5ae8dd9d4663
Suyash <102641930+suy-lab@users.noreply.github.com>
2023-03-24 19:54:49 +0530

    Update rock_paper_scissor.py
    added some other style in comment 👍

--- rock_paper_scissor.py
+++ rock_paper_scissor.py
@@ -1,4 +1,3 @@
-from operator import truediv
 import random
 
 def play():
@@ -30,3 +29,53 @@ while(y=='y'):
     y='n'
     print(play())
     y=input("do you want to do more (y/n)")
+ 
+
+
+"""
+# Another method for the same
+
+import random as r
+
+
+l=r.randint(0,2)
+#print(l)
+
+
+p=int(input("""Enter the choice
+               0 rock
+               1 paper
+               2 sessior """))
+
+if p==0:
+    print('Your Choice is rock')
+elif p==1:
+    print('Your Choice is paper')
+elif p==2:
+    print('Your Choice is sessior')
+
+    
+    
+if l==0:
+    print('Computer Choice is rock')
+elif l==1:
+    print('Computer Choice is paper')
+elif l==2:
+    print('Computer Choice is sessior')
+    
+
+
+
+if p==l:
+    print('Game Draw!!!')
+elif p==0 and l==2:
+    print('You win the game!!!')
+elif l==0 and p==2:
+    print('You loss the game!!!')
+elif l>p:
+    print('You loss the game!!!')
+elif p>l:
+    print('You win the game!!!')
+
+
+"""
